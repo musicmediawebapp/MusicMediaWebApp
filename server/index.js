b97205f4a075518a543ae783a4915ef776bbea4d
Minh Lu <minh@socedo.com>
2017-10-04 18:46:17 -0700

    Added PW file to gitignore, set up MongoDB and mongoose

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
+var keys = require('../config/keys');
 var express = require('express');
 /* We are using a single instance of Express to handle incoming requests */
 var app = express();
+
+/* MongoDB set up */
+var mongoose = require('mongoose');
+mongoose.connect(keys.mongoURI);
 
 /* Dummy router example */
 app.get('/api/test', (req, res) => {
