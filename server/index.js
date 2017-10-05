a8980887e35c3c6c4dc9eb5a51467b23251e2225
Minh Lu <minh@socedo.com>
2017-10-04 18:59:42 -0700

    Added dummy model and db query

--- server/index.js
+++ server/index.js
@@ -1,3 +1,6 @@
+/* Model imports */
+require('./models/user');
+
 var keys = require('../config/keys');
 var express = require('express');
 /* We are using a single instance of Express to handle incoming requests */
@@ -6,10 +9,13 @@ var app = express();
 /* MongoDB set up */
 var mongoose = require('mongoose');
 mongoose.connect(keys.mongoURI);
+var User = mongoose.model('users');
 
 /* Dummy router example */
-app.get('/api/test', (req, res) => {
-    res.send({ test: 'test' });
+app.get('/api/test', async (req, res) => {
+    var user = await User.findOne({ Id: 123 });
+
+    res.send({ user });
 });
 
 /* Instruct Express to listen to this port */
