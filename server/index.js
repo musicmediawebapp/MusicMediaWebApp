1e30bbacb192b92f21b697b94db0479b977443d6
Minh Lu <minh@socedo.com>
2017-10-04 18:25:00 -0700

    Added react front-end and made a sample frontend to backend communication. Also set up cmd to spin up 2 servers

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ var express = require('express');
 var app = express();
 
 /* Dummy router example */
-app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+app.get('/api/test', (req, res) => {
+    res.send({ test: 'test' });
 });
 
 /* Instruct Express to listen to this port */
