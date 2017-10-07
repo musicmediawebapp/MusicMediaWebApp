6ab843ee4d42e6a9fab216e84430282417780609
Minh Lu <minh@socedo.com>
2017-10-06 18:23:01 -0700

    Added landing page boilerplate

---
+++ client/src/components/Landing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+    render() {
+        return(
+            <div>
+                Landing page!
+            </div>
+        );
+    }
+}
+
+export default Landing;
