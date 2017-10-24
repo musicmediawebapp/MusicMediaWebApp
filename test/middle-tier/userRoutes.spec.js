6a62fab52a080283e31617a3d31b027cbd0e6bc3
Minh Lu <minh@socedo.com>
2017-10-23 21:28:06 -0700

    Added message to dummy test

--- test/middle-tier/userRoutes.spec.js
+++ test/middle-tier/userRoutes.spec.js
@@ -7,6 +7,6 @@ describe('Array', function() {
     it('should start empty', function() {
         var arr = [];
 
-        assert.equal(arr.length, 0);
+        assert.equal(arr.length, 0, "Array length was not 0");
     });
 });
