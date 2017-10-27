a0867812a7e0d8057cc5b36e0f519441ce543370
Minh Lu <minh@socedo.com>
2017-10-26 22:26:20 -0700

    Added toast CSS into the style loader (webpack) in index.js, made the toast in the component and tested it

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-import Materialize from 
+import { ToastContainer, toast } from 'react-toastify';
 
 class Dashboard extends Component {
 
     componentDidMount() {
-        var { finishedWorkflow } = this.props.location.state;
+        var { location } = this.props;
+        if (location.state && location.state.finishedWorkflow) {
+            toast("You have completed the setup!");
+        }
     }
 
     render() {
         return (
             <div>
                 <h2>Dashboard</h2>
+
+                {/* Toast if the user just finished the workflow */}
+                <ToastContainer 
+                    position="top-right"
+                    type="default"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    pauseOnHover
+                />
             </div>
         );
     }
