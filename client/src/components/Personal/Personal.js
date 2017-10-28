7ecce73aee0884effe1a36f9c7d555d20e43dc32
Minh Lu <minh@socedo.com>
2017-10-27 18:15:58 -0700

    Made new FormFields for Personal component, moved RenderFIeld since it's used in other places and connected Personal.js to redux-form

--- client/src/components/Personal/Personal.js
+++ client/src/components/Personal/Personal.js
@@ -1,14 +1,52 @@
 import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import validate from '../utils/ValidateWorkflow';
+import formFields from './FormFields';
+import _ from 'lodash';
+import { fetchUser } from '../../actions/index';
+import { connect } from 'react-redux';
 
 class Personal extends Component {
+    renderFields() {
+        return _.map(formFields, ({ key, type, label, name, component }) => {
+            return (
+                <Field 
+                    key={key}
+                    type={type}
+                    label={label}
+                    name={name}
+                    component={component.RenderField}
+                />
+            );
+        });
+    }
+
     render() {
-        console.log("Profile rendered");
-        return (
+        var { handleSubmit } = this.props;
+        return(
             <div>
-                This is your profile!
+                <form onSubmit={handleSubmit}>
+                    {this.renderFields()}
+                    <button className="teal btn-flat right white-text" type="submit">
+                        Next
+                        <i className="material-icons right">done</i>
+                    </button>
+                </form>
             </div>
         );
     }
 }
+
+Personal = reduxForm({
+    form: 'personalForm',
+    validate
+})(Personal);
+
+Personal = connect(
+    state => ({
+        initialValues: state.auth // pull initial values from auth reducer
+    }),
+    {load: fetchUser} // bind loading action creator
+)(Personal);
 
 export default Personal;
