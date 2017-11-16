a4bb8093fa0c83cd984a05acaf3692f5cf7d03df
trilxs <tamlumusic@gmail.com>
2017-11-16 01:48:19 -0800

    Started working on music player UI in the dashboard

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import { toast } from 'react-toastify';
 import completeWorkflowToast from '../../utils/Toasts/default';
 import * as actions from '../../actions/index';
 import { connect } from 'react-redux';
+import './Dashboard.css';
 
 class Dashboard extends Component {
 
@@ -11,7 +12,7 @@ class Dashboard extends Component {
         this.props.showHeader();
         // Update the user model so that components like the Profile can pre-load the up-to-date model
         this.props.fetchUser();
-        
+
         var { location } = this.props;
         // Toast if the user just finished the workflow
         if (location.state && location.state.finishedWorkflow) {
@@ -21,9 +22,27 @@ class Dashboard extends Component {
 
     render() {
         return (
-            <div className="container">
-                <h2>Dashboard</h2>
+            <div className="container-bg">      
                 {completeWorkflowToast}
+
+                <div className="carousel-suggested-container-container">
+                        <div className="carousel-suggested-container">
+                            <div className="background-gradient">
+                                <div className="song-player-container">
+                                    <div className="artwork-wrapper">
+                                        <img src={require('../../assets/album-temp.png')}/>
+                                    </div>
+                                </div>
+                                <div className="username-title-container"> 
+                                    <div className="track-title">Shipwreck - The Coast</div>
+                                    <div className="track-username">testuser201</div>
+                                </div> 
+                                <div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                </div>
             </div>
         );
     }
